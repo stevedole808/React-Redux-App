c93b1aeab7affd956ec2877c420a333c98664526
stevedole808 <stevedolelambda@gmail.com>
2019-12-12 10:53:38 -1000

    initial commit

---
+++ my-app/src/redux/components/business.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Business =  props => {
+    return (
+        <div>
+        <h1>Jokes are sick</h1>
+        </div>
+    )
+}
+
+const mapStateToProp = state => {
+    return {
+
+    }
+}
+
